138c80008544eabd6ad635e2fbd26074c68fefe8
yongseng.chia@isatec.com <yongseng.chia@isatec.com>
2024-01-01 09:53:44 +0800

    Eng of Section 7

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,26 +1,30 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using API.Data;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Authorization;
+
 namespace API.Controllers;
 
-[ApiController]
-[Route("api/[controller]")]
-public class UsersController : ControllerBase
+[Authorize]
+public class UsersController : BaseApiController
 {
-private readonly DataContext _context;
-public UsersController(DataContext context)
-{
-    _context = context;
-}
+    private readonly DataContext _context;
+    public UsersController(DataContext context)
+    {
+        _context = context;
+    }
 
-[HttpGet]
-public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers(){
-    var users = await _context.Users.ToListAsync();
-    return users;
-}
+    [AllowAnonymous]
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+    {
+        var users = await _context.Users.ToListAsync();
+        return users;
+    }
 
-[HttpGet("{id}")]
-public async Task<ActionResult<AppUser>> GetUser( int id){
-    return await _context.Users.FindAsync(id);
-}
+    [HttpGet("{id}")]
+    public async Task<ActionResult<AppUser>> GetUser(int id)
+    {
+        return await _context.Users.FindAsync(id);
+    }
 }
--- API/Program.cs
+++ API/Program.cs
@@ -1,22 +1,31 @@
-using API.Controllers;
-using API.Data;
-using Microsoft.EntityFrameworkCore;
+using API.Extensions;
+using API.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-builder.Services.AddDbContext<DataContext>(opt => 
-    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"))
-    ); 
-    
+
+builder.Services.AddApplicationServices(builder.Configuration);
+
+builder.Services.AddIdentityServices(builder.Configuration);
+
+
 var app = builder.Build();
 
+// Configure the HTTP request pipeline. (Middleware)
+app.UseMiddleware<ExceptionMiddleWare>();
 app.UseHttpsRedirection();
 
+app.UseCors(options => options.WithOrigins("https://localhost:4200")
+.AllowAnyMethod()
+.AllowAnyHeader());
+
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
 
 app.Run();
+ 
